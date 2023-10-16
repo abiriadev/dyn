4b58b63d30dc880107c49e02f89a22f987efe338
Abiria <abiria.dev@gmail.com>
2023-10-16 16:53:52 +0900

    feat: add lexer

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,14 +1,101 @@
-pub fn add(left: usize, right: usize) -> usize {
-	left + right
-}
+use logos::Logos;
 
-#[cfg(test)]
-mod tests {
-	use super::*;
+#[derive(Debug, PartialEq, Logos)]
+#[logos(skip r"[ \t\n]+")]
+enum Token {
+	#[token("+")]
+	Plus,
 
-	#[test]
-	fn it_works() {
-		let result = add(2, 2);
-		assert_eq!(result, 4);
-	}
+	#[token("-")]
+	Minus,
+
+	#[token("*")]
+	Asterisk,
+
+	#[token("/")]
+	Slash,
+
+	#[token("=")]
+	Equal,
+
+	#[token("!")]
+	Bang,
+
+	#[token(",")]
+	Comma,
+
+	#[token("@")]
+	At,
+
+	#[token("'")]
+	SingleQuote,
+
+	#[token("\"")]
+	DoubleQuote,
+
+	#[token("(")]
+	LeftParenthesis,
+
+	#[token(")")]
+	RightParenthesis,
+
+	#[token("{")]
+	LeftBrace,
+
+	#[token("}")]
+	RightBrace,
+
+	#[token("[")]
+	LeftBracket,
+
+	#[token("]")]
+	RightBracket,
+
+	#[token("+=")]
+	PlusEqual,
+
+	#[token("-=")]
+	MinusEqual,
+
+	#[token("*=")]
+	AsteriskEqual,
+
+	#[token("/=")]
+	SlashEqual,
+
+	#[token("&&")]
+	DoubleAnd,
+
+	#[token("||")]
+	DoublePipe,
+
+	#[token("->")]
+	Arrow,
+
+	#[token("let")]
+	Let,
+
+	#[token("let!")]
+	LetMut,
+
+	#[token("if")]
+	If,
+
+	#[token("else")]
+	Else,
+
+	#[token("iter")]
+	Iter,
+
+	#[token("of")]
+	Of,
+
+	#[token("import")]
+	Import,
+
+	#[token("export")]
+	Export,
+
+	#[regex("[a-zA-Z]+")]
+	Identifier,
 }
