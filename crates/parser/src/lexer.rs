52bec00313e130bab123a9027eadcd7b55d399e7
Abiria <abiria.dev@gmail.com>
2023-10-18 14:58:51 +0900

    feat: make `SpannedIterator` actually iterable

--- crates/parser/src/lexer.rs
+++ crates/parser/src/lexer.rs
@@ -6,6 +6,8 @@ mod token;
 
 #[cfg(test)] mod tests;
 
+pub type Spanned<Tok, Loc, E> = Result<(Loc, Tok, Loc), E>;
+
 #[derive(Default, Debug, Clone, PartialEq)]
 pub enum LexError {
 	InvalidIndentifier,
@@ -18,4 +20,14 @@ pub struct SpannedLexer<'a>(SpannedIter<'a, Token>);
 
 impl<'a> SpannedLexer<'a> {
 	pub fn new(code: &'a str) -> Self { Self(Token::lexer(code).spanned()) }
+}
+
+impl<'a> Iterator for SpannedLexer<'a> {
+	type Item = Spanned<Token, usize, LexError>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.0
+			.next()
+			.map(|(token, span)| token.map(|t| (span.start, t, span.end)))
+	}
 }
