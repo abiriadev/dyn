1cd85afdcefb07a5ae5477078e60a58f43d2bb49
Abiria <abiria.dev@gmail.com>
2023-10-24 10:25:52 +0900

    feat: implement `Box` detection logic

--- crates/box-tt/src/box_new.rs
+++ crates/box-tt/src/box_new.rs
@@ -1,6 +1,38 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse2, Data, DeriveInput, Field, Fields};
+use syn::{
+	parse2, Data, DeriveInput, Field, Fields, GenericArgument, PathArguments,
+	Type,
+};
+
+enum BoxOrNot<T> {
+	Box(T),
+	Not(T),
+}
+
+fn unwrap_box(ty: &Type) -> BoxOrNot<&Type> {
+	let Type::Path(path) = ty else {
+		return BoxOrNot::Not(ty);
+	};
+
+	let Some(seg) = path.path.segments.first() else {
+		return BoxOrNot::Not(ty);
+	};
+
+	if seg.ident.to_string() != "Box" {
+		return BoxOrNot::Not(ty);
+	};
+
+	let PathArguments::AngleBracketed(ang) = seg.arguments else {
+		return BoxOrNot::Not(ty);
+	};
+
+	let Some(GenericArgument::Type(ty)) = ang.args.first() else {
+		return BoxOrNot::Not(ty);
+	};
+
+	BoxOrNot::Box(ty)
+}
 
 pub fn box_new(input: TokenStream) -> TokenStream {
 	let input = match parse2::<DeriveInput>(input) {
@@ -22,17 +54,14 @@ pub fn box_new(input: TokenStream) -> TokenStream {
 		named
 			.named
 			.iter()
-			.filter_map(|Field { ident, ty, .. }| {
-				if let Some(i) = ident {
-					Some((i, ty))
-				} else {
-					None
-				}
+			.filter_map(|Field { ident, ty, .. }| match ident {
+				Some(i) => Some((i, unwrap_box(ty))),
+				None => None,
 			});
 
-	let args = fields
-		.clone()
-		.map(|(ident, ty)| quote! { #ident: #ty });
+	let args = fields.clone().map(|(ident, mut ty)| {
+		quote! { #ident: #ty }
+	});
 
 	let body = fields.map(|(ident, _)| quote! { #ident: #ident});
 
