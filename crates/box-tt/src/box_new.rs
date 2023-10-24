c07a1230d91b5f22128682faab21af7053c25aaa
Abiria <abiria.dev@gmail.com>
2023-10-24 09:09:03 +0900

    feat: parse `Derive` input

--- crates/box-tt/src/box_new.rs
+++ crates/box-tt/src/box_new.rs
@@ -1,3 +1,16 @@
 use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse2, DeriveInput};
 
-pub fn box_new(input: TokenStream) -> TokenStream { input }
+pub fn box_new(input: TokenStream) -> TokenStream {
+	let input = match parse2::<DeriveInput>(input) {
+		Ok(v) => v,
+		Err(e) => return e.to_compile_error(),
+	};
+
+	println!("{input:#?}");
+
+	quote! {
+		// impl
+	}
+}
