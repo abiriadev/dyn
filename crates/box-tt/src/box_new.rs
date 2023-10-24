f2881b250d69d60ec4f629f89e712dfcb0a3531e
Abiria <abiria.dev@gmail.com>
2023-10-24 11:45:09 +0900

    feat: implment generator logic for `enum`

--- crates/box-tt/src/box_new.rs
+++ crates/box-tt/src/box_new.rs
@@ -1,10 +1,11 @@
 use either::Either::{self, Left, Right};
-use proc_macro2::{Ident, TokenStream};
-use quote::quote;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::{format_ident, quote};
 use syn::{
-	parse2, token::Struct, AngleBracketedGenericArguments, Data, DataStruct,
-	DeriveInput, Field, Fields, GenericArgument, Path, PathArguments,
-	PathSegment, Type, TypePath,
+	parse2, punctuated::Punctuated, token::Comma,
+	AngleBracketedGenericArguments, Data, DataEnum, DataStruct, DeriveInput,
+	Field, Fields, FieldsUnnamed, GenericArgument, Path, PathArguments,
+	PathSegment, Type, TypePath, Variant,
 };
 
 fn unwrap_box(ty: &Type) -> Either<&Type, &Type> {
@@ -49,7 +50,7 @@ pub fn box_new(input: TokenStream) -> TokenStream {
 
 	match input.data {
 		Data::Struct(DataStruct { fields, .. }) => box_struct(ident, fields),
-		Data::Enum(_) => unimplemented!(),
+		Data::Enum(DataEnum { variants, .. }) => box_enum(ident, variants),
 		Data::Union(_) => unimplemented!(),
 	}
 }
@@ -89,6 +90,56 @@ fn box_struct(ident: Ident, fields: Fields) -> TokenStream {
 					#(#body),*
 				}
 			}
+		}
+	}
+}
+
+fn box_enum(ident: Ident, variants: Punctuated<Variant, Comma>) -> TokenStream {
+	println!("{:#?}", variants);
+
+	let methods =
+		variants
+			.iter()
+			.filter_map(|Variant { ident, fields, .. }| {
+				if let Fields::Unnamed(FieldsUnnamed { unnamed, .. }) = fields {
+					let method = format_ident!("{ident}_box");
+
+					let fields = unnamed.iter().enumerate().map(
+						|(i, Field { ty, .. })| {
+							(
+								Ident::new(&format!("i{i}"), Span::call_site()),
+								unwrap_box(ty),
+							)
+						},
+					);
+
+					let args = fields.clone().map(|(ident, ty)| {
+						let ty = ty.into_inner();
+
+						quote! { #ident: #ty }
+					});
+
+					let body = fields.map(|(ident, ty)| {
+						ty.map_either(
+							|_| quote! {#ident},
+							|_| quote! {Box::new(#ident)},
+						)
+						.into_inner()
+					});
+
+					Some(quote! {
+						fn #method(#(#args),*) -> Self {
+							Self::#ident(#(#body),*)
+						}
+					})
+				} else {
+					None
+				}
+			});
+
+	quote! {
+		impl #ident {
+			#(#methods)*
 		}
 	}
 }
