fcbabecdb4cce5f3f48a3438a95215a524c666dc
Abiria <abiria.dev@gmail.com>
2023-10-24 09:04:23 +0900

    feat: add abstracted implementation with `proc_macro2`

---
+++ crates/box-tt/src/box_new.rs
@@ -0,0 +1,3 @@
+use proc_macro2::TokenStream;
+
+pub fn box_new(input: TokenStream) -> TokenStream { input }
--- crates/box-tt/src/lib.rs
+++ crates/box-tt/src/lib.rs
@@ -3,4 +3,4 @@ use proc_macro::TokenStream;
 mod box_new;
 
 #[proc_macro_derive(BoxNew)]
-pub fn box_new(input: TokenStream) -> TokenStream { input }
+pub fn box_new(input: TokenStream) -> TokenStream { box_new::box_new(input.into()).into() }
