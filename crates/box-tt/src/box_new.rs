58cadacc295f6aa19cfcce5dce61d30414f47d03
Abiria <abiria.dev@gmail.com>
2023-10-24 09:47:12 +0900

    feat: implement generation logic

--- crates/box-tt/src/box_new.rs
+++ crates/box-tt/src/box_new.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse2, DeriveInput};
+use syn::{parse2, Data, DeriveInput, Field, Fields};
 
 pub fn box_new(input: TokenStream) -> TokenStream {
 	let input = match parse2::<DeriveInput>(input) {
@@ -10,7 +10,46 @@ pub fn box_new(input: TokenStream) -> TokenStream {
 
 	println!("{input:#?}");
 
+	let ident = input.ident;
+
+	let Data::Struct(s) = input.data else {
+		panic!()
+	};
+
+	let Fields::Named(named) = s.fields else {
+		panic!()
+	};
+
+	let fields =
+		named
+			.named
+			.iter()
+			.filter_map(|Field { ident, ty, .. }| {
+				if let Some(i) = ident {
+					Some((i, ty))
+				} else {
+					None
+				}
+			});
+
+	let args = fields
+		.clone()
+		.map(|(ident, ty)| quote! { #ident: #ty });
+
+	println!(
+		"{:#?}",
+		fields.clone().collect::<Vec<_>>()
+	);
+
+	let body = fields.map(|(ident, _)| quote! { #ident: #ident});
+
 	quote! {
-		// impl
+		impl #ident {
+			fn new_box(#(#args),*) -> Self {
+				Self {
+					#(#body),*
+				}
+			}
+		}
 	}
 }
