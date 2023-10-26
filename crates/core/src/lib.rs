ed10f2d8eafd0ba5fec2765358b916ac1b137de9
Abiria <abiria.dev@gmail.com>
2023-10-26 22:30:40 +0900

    feat: define basic `Environment`

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,1 +1,31 @@
-//
+use std::collections::{hash_map::Entry, HashMap};
+
+use parser::ast::Ident;
+
+pub enum ReferenceError {
+	UndefinedIdentifier,
+}
+
+pub enum RuntimeError {
+	ReferenceError(ReferenceError),
+	AssignmentToImmutableVariable,
+}
+
+pub enum Value {
+	Integer(i32),
+	String(String),
+	Array(Vec<Value>),
+}
+
+struct SymbolInfo {
+	mutable: bool,
+	value: Value,
+}
+
+pub struct Environment {
+	store: HashMap<Ident, SymbolInfo>,
+}
+
+impl Environment {}
+
+pub struct Interpreter {}
