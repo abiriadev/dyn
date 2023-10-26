a931cee7fcd476592e414bd7833f0ef4d3fe8b5a
Abiria <abiria.dev@gmail.com>
2023-10-26 23:11:25 +0900

    feat: implement `Environment`

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,4 +1,7 @@
-use std::collections::{hash_map::Entry, HashMap};
+use std::collections::{
+	hash_map::{Entry, OccupiedEntry},
+	HashMap,
+};
 
 use parser::ast::Ident;
 
@@ -26,6 +29,50 @@ pub struct Environment {
 	store: HashMap<Ident, SymbolInfo>,
 }
 
-impl Environment {}
+impl Environment {
+	fn entry(
+		&mut self,
+		ident: Ident,
+	) -> Result<OccupiedEntry<'_, Ident, SymbolInfo>, RuntimeError> {
+		match self.store.entry(ident) {
+			Entry::Occupied(o) => Ok(o),
+			Entry::Vacant(_) => Err(RuntimeError::ReferenceError(
+				ReferenceError::UndefinedIdentifier,
+			)),
+		}
+	}
+
+	pub fn declare(
+		&mut self,
+		ident: Ident,
+		value: Value,
+		mutable: bool,
+	) -> Result<(), RuntimeError> {
+		self.entry(ident)?
+			.insert(SymbolInfo { mutable, value });
+		Ok(())
+	}
+
+	pub fn assign(
+		&mut self,
+		ident: Ident,
+		value: Value,
+	) -> Result<(), RuntimeError> {
+		let mut e = self.entry(ident)?;
+		let sym = e.get_mut();
+
+		if !sym.mutable {
+			return Err(RuntimeError::AssignmentToImmutableVariable);
+		}
+
+		sym.value = value;
+		Ok(())
+	}
+
+	pub fn free(&mut self, ident: Ident) -> Result<(), RuntimeError> {
+		self.entry(ident)?.remove();
+		Ok(())
+	}
+}
 
 pub struct Interpreter {}
