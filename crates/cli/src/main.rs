495677d3d096e6d45caeb2dc7f271a120d0ae209
Abiria <abiria.dev@gmail.com>
2023-10-27 03:56:27 +0900

    feat: implement basic interpreter CLI

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-	println!("Hello, world!");
+use std::{fs::read_to_string, path::PathBuf};
+
+use clap::Parser;
+use dyn_core::Interpreter;
+
+#[derive(Debug, Parser)]
+struct Args {
+	source_path: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+	let args = Args::parse();
+
+	let source = read_to_string(args.source_path)?;
+
+	let mut intpr = Interpreter::init();
+	let res = intpr.run(&source);
+
+	println!("final result: {:?}", res);
+	Ok(())
 }
