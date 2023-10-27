253a3eae4829cd6dc51fb752f429a4c01424c12f
Abiria <abiria.dev@gmail.com>
2023-10-27 17:36:51 +0900

    feat: implement 'real' stdout printer for cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,11 +1,29 @@
-use std::{fs::read_to_string, path::PathBuf};
+use std::{collections::HashMap, fs::read_to_string, path::PathBuf};
 
 use clap::Parser;
-use dyn_core::Interpreter;
+use dyn_core::{
+	ArgumentValues, BuiltinFunction, FunctionValue, Interpreter, Value,
+};
+use parser::ast::Ident;
 
 #[derive(Debug, Parser)]
 struct Args {
 	source_path: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+struct Printer;
+
+impl Printer {
+	fn new() -> Box<Self> { Box::new(Self) }
+}
+
+impl BuiltinFunction for Printer {
+	fn call(&mut self, args: ArgumentValues) -> Value {
+		let msg = args.0.into_iter().nth(0).unwrap();
+		println!("{}", msg.clone());
+		msg
+	}
 }
 
 fn main() -> anyhow::Result<()> {
@@ -13,7 +31,12 @@ fn main() -> anyhow::Result<()> {
 
 	let source = read_to_string(args.source_path)?;
 
-	let mut intpr = Interpreter::init();
+	let mut h = HashMap::new();
+	h.insert(
+		Ident("print".to_owned()),
+		Value::Function(FunctionValue::Builtin(Printer::new())),
+	);
+	let mut intpr = Interpreter::init_with_builtins(h).unwrap();
 	let res = intpr.run(&source);
 
 	println!("final result: {:?}", res);
