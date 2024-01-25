389d3ccdfe5aaaeb322ede0667d3f25845b8a5ee
Abiria <abiria.dev@gmail.com>
2024-01-25 18:51:54 +0900

    feat: define basic lsp

--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-	println!("Hello, world!");
+use tower_lsp::{
+	jsonrpc,
+	lsp_types::{
+		InitializeParams, InitializeResult, InitializedParams, MessageType,
+	},
+	Client, LanguageServer, LspService, Server,
+};
+
+#[derive(Debug)]
+struct Backend {
+	client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+	async fn initialize(
+		&self,
+		_: InitializeParams,
+	) -> jsonrpc::Result<InitializeResult> {
+		Ok(InitializeResult::default())
+	}
+
+	async fn initialized(&self, _: InitializedParams) {
+		self.client
+			.log_message(MessageType::INFO, "server initialized!")
+			.await;
+	}
+
+	async fn shutdown(&self) -> jsonrpc::Result<()> { Ok(()) }
+}
+
+#[tokio::main]
+async fn main() {
+	let stdin = tokio::io::stdin();
+	let stdout = tokio::io::stdout();
+
+	let (service, socket) = LspService::new(|client| Backend { client });
+	Server::new(stdin, stdout, socket)
+		.serve(service)
+		.await;
 }
