00e0dd4a42479778f143fa8eb1608b476baee48f
Abiria <abiria.dev@gmail.com>
2024-02-07 16:46:06 +0900

    feat: implement nested block comment

--- crates/lexer/src/lexer/comment.rs
+++ crates/lexer/src/lexer/comment.rs
@@ -1,4 +1,8 @@
-use winnow::{ascii::till_line_ending, PResult, Parser};
+use winnow::{
+	ascii::till_line_ending,
+	combinator::{alt, delimited, not},
+	PResult, Parser,
+};
 
 use super::I;
 use crate::Token;
@@ -7,4 +11,17 @@ pub fn line_comment(i: &mut I<'_>) -> PResult<Token> {
 	("//", till_line_ending)
 		.value(Token::LineComment)
 		.parse_next(i)
+}
+
+pub fn block_comment(i: &mut I<'_>) -> PResult<Token> {
+	delimited(
+		"/*",
+		alt((
+			not(alt(("/*", "*/"))),
+			block_comment.void(),
+		)),
+		"*/",
+	)
+	.value(Token::BlockComment)
+	.parse_next(i)
 }
