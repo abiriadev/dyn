20464dbe3c71d5ae8c23317e4af670b8f243369d
Abiria <abiria.dev@gmail.com>
2024-02-08 12:50:11 +0900

    feat: aggregate all the parsers so far

--- crates/lexer/src/lexer.rs
+++ crates/lexer/src/lexer.rs
@@ -8,11 +8,22 @@ mod keyword;
 mod string;
 mod whitespace;
 
+use comment::{block_comment, line_comment};
 use identifier::identifier;
 use keyword::keyword;
+use string::string;
+use whitespace::whitespace;
 
 type Stream<'a> = Located<&'a str>;
 
 pub fn token(i: &mut Stream<'_>) -> PResult<Token> {
-	alt((keyword, identifier)).parse_next(i)
+	alt((
+		whitespace,
+		line_comment,
+		block_comment,
+		keyword,
+		identifier,
+		string,
+	))
+	.parse_next(i)
 }
