a98333757b9fb5e85b56400beb25389271b2446a
Abiria <abiria.dev@gmail.com>
2024-02-09 19:03:34 +0900

    feat: use `token` parser

--- crates/lexer/src/lexer.rs
+++ crates/lexer/src/lexer.rs
@@ -1,6 +1,13 @@
-use winnow::{combinator::alt, Located, PResult, Parser};
+use std::ops::Range;
 
-use crate::Token;
+use span::{Span, Spanned};
+use winnow::{
+	combinator::{alt, cut_err},
+	error::ErrMode,
+	Located, PResult, Parser,
+};
+
+use crate::{LexError, Token};
 
 mod comment;
 mod identifier;
@@ -26,4 +33,32 @@ pub fn token(i: &mut Stream<'_>) -> PResult<Token> {
 		string,
 	))
 	.parse_next(i)
+}
+
+pub type SpannedToken<Tok, Loc, E> = Result<(Loc, Tok, Loc), E>;
+
+pub struct SpannedLexer<'a>(Located<&'a str>);
+
+impl<'a> SpannedLexer<'a> {
+	pub fn new(code: &'a str) -> Self { Self(Located::new(code)) }
+}
+
+impl<'a> Iterator for SpannedLexer<'a> {
+	type Item = SpannedToken<Token, usize, Spanned<LexError>>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match cut_err(token)
+			.with_span()
+			.parse_next(&mut self.0)
+		{
+			Ok((tok, Range { start, end })) => Some(Ok((start, tok, end))),
+			Err(err) => {
+				let err = err.into_inner();
+				Some(Err(Spanned::new(
+					Span::DUMMY_SPAN,
+					LexError::InvalidToken,
+				)))
+			},
+		}
+	}
 }
--- crates/lexer/src/lib.rs
+++ crates/lexer/src/lib.rs
@@ -8,9 +8,7 @@ mod token;
 use span::Spanned;
 pub use token::{QuotedString, Token};
 
-#[cfg(test)] mod tests;
-
-pub type SpannedToken<Tok, Loc, E> = Result<(Loc, Tok, Loc), E>;
+// #[cfg(test)] mod tests;
 
 #[derive(Default, Debug, Clone, PartialEq, Error)]
 pub enum LexError {
@@ -20,23 +18,4 @@ pub enum LexError {
 	#[default]
 	#[error("InvalidToken")]
 	InvalidToken,
-}
-
-pub struct SpannedLexer<'a>(PhantomData<&'a ()>);
-
-impl<'a> SpannedLexer<'a> {
-	pub fn new(code: &'a str) -> Self { todo!() }
-}
-
-impl<'a> Iterator for SpannedLexer<'a> {
-	type Item = SpannedToken<Token, usize, Spanned<LexError>>;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		// self.0.next().map(|(token, span)| {
-		// 	token
-		// 		.map(|t| (span.start, t, span.end))
-		// 		.map_err(|e| Spanned::new(span, e))
-		// })
-		todo!()
-	}
 }
