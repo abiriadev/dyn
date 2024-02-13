2b743223de85e628405ebb6ff0bc416749e030f8
Abiria <abiria.dev@gmail.com>
2024-02-13 17:24:51 +0900

    fix: handle EOF properly

--- crates/lexer/src/lexer.rs
+++ crates/lexer/src/lexer.rs
@@ -2,7 +2,7 @@ use std::ops::Range;
 
 use span::{Span, Spanned};
 use winnow::{
-	combinator::{alt, cut_err},
+	combinator::{alt, cut_err, eof},
 	Located, PResult, Parser,
 };
 
@@ -49,13 +49,16 @@ impl<'a> Iterator for SpannedLexer<'a> {
 	type Item = SpannedToken<Token, usize, Spanned<LexError>>;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		match cut_err(token)
+		match alt((eof.value(None), token.map(Some)))
 			.with_span()
 			.parse_next(&mut self.0)
 		{
-			Ok((tok, Range { start, end })) => Some(Ok((start, tok, end))),
+			Ok((Some(tok), Range { start, end })) =>
+				Some(Ok((start, tok, end))),
+			Ok((None, _)) => None,
 			Err(err) => {
 				let err = err.into_inner();
+				println!("{:?}", err);
 				Some(Err(Spanned::new(
 					Span::DUMMY_SPAN,
 					LexError::InvalidToken,
