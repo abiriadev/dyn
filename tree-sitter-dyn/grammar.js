6af4d59732342774d7903471350539c51e9c9542
Abiria <abiria.dev@gmail.com>
2024-03-08 09:46:18 +0900

    feat: add basic precedence

--- tree-sitter-dyn/grammar.js
+++ tree-sitter-dyn/grammar.js
@@ -5,11 +5,15 @@ module.exports = grammar({
 	name: 'dyn',
 
 	rules: {
-		source_file: $ => $.code,
+		source_file: $ => repeat($.expr),
 
 		code: $ => repeat($.expr),
 
-		expr: $ => choice($.identifier, $.number),
+		expr: $ =>
+			choice($.identifier, $.number, $.binexpr),
+
+		binexpr: $ =>
+			choice(prec.left(1, seq($.expr, '+', $.expr))),
 
 		identifier: $ => /[a-z]+/,
 
