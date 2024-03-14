d996f8472d900342a975851f5eebc1489fa6456c
Abiria <abiria.dev@gmail.com>
2024-03-14 17:13:52 +0900

    feat: define basic types

--- crates/core/src/environment.rs
+++ crates/core/src/environment.rs
@@ -1,1 +1,22 @@
+use std::{
+	collections::HashMap,
+	sync::{Arc, RwLock},
+};
 
+use dyn_parser::ast::Ident;
+
+use crate::SymbolInfo;
+
+type BindTable = HashMap<Ident, SymbolInfo>;
+type Arw<T> = Arc<RwLock<T>>;
+
+struct Scope(BindTable);
+
+struct Frame {
+	scope_stack: Vec<Scope>,
+	parent: Option<Arw<Frame>>,
+}
+
+struct Environment {
+	call_stack: Arw<Frame>,
+}
