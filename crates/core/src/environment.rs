fb4d446eb6f6d8c3888d812525170aba684c5693
Abiria <abiria.dev@gmail.com>
2024-03-14 17:24:02 +0900

    fix: syntax error

--- crates/core/src/environment.rs
+++ crates/core/src/environment.rs
@@ -11,7 +11,7 @@ type BindTable = HashMap<Ident, SymbolInfo>;
 type Arw<T> = Arc<RwLock<T>>;
 type ArwFrame = Arw<Frame>;
 type RuntimeResult<T> = Result<T, RuntimeError>;
-type IndexedStack; = Vec<T>;
+type IndexedStack = Vec<T>;
 
 struct Scope(BindTable);
 
@@ -29,31 +29,39 @@ impl Environment {
 		todo!()
 	}
 
-	pub fn declare(ident: &Ident, value: Value, is_mut: bool) -> RuntimeResult<()> {
-        todo!()
-    }
+	pub fn declare(
+		ident: &Ident,
+		value: Value,
+		is_mut: bool,
+	) -> RuntimeResult<()> {
+		todo!()
+	}
 
 	pub fn assign(ident: &Ident, value: Value) -> RuntimeResult<()> {
-        todo!()
-    }
+		todo!()
+	}
 
 	pub fn load(ident: &Ident) -> RuntimeResult<Value> {
-        todo!()
-    }
+		todo!()
+	}
 
-	pub fn call(capture: ArwFrame, parameters: Parameters, arguments: Arguments) {
-        todo!()
-    }
+	pub fn call(
+		capture: ArwFrame,
+		parameters: Parameters,
+		arguments: Arguments,
+	) {
+		todo!()
+	}
 
 	pub fn ret() {
-        todo!()
-    }
+		todo!()
+	}
 
 	pub fn push_scope() {
-        todo!()
-    }
+		todo!()
+	}
 
 	pub fn pop_scope() {
-        todo!()
-    }
+		todo!()
+	}
 }
