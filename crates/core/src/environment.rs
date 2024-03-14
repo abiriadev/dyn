de496629aae187f9dd9ac418292d1bee2e41a1ba
Abiria <abiria.dev@gmail.com>
2024-03-14 17:22:10 +0900

    feat: define basic backbone

--- crates/core/src/environment.rs
+++ crates/core/src/environment.rs
@@ -3,20 +3,57 @@ use std::{
 	sync::{Arc, RwLock},
 };
 
-use dyn_parser::ast::Ident;
+use dyn_parser::ast::{Arguments, Ident, Parameters};
 
-use crate::SymbolInfo;
+use crate::{RuntimeError, SymbolInfo};
 
 type BindTable = HashMap<Ident, SymbolInfo>;
 type Arw<T> = Arc<RwLock<T>>;
+type ArwFrame = Arw<Frame>;
+type RuntimeResult<T> = Result<T, RuntimeError>;
+type IndexedStack; = Vec<T>;
 
 struct Scope(BindTable);
 
 struct Frame {
-	scope_stack: Vec<Scope>,
-	parent: Option<Arw<Frame>>,
+	scope_stack: IndexedStack<Scope>,
+	parent: Option<ArwFrame>,
 }
 
-struct Environment {
-	call_stack: Arw<Frame>,
+pub struct Environment {
+	call_stack: ArwFrame,
+}
+
+impl Environment {
+	pub fn new() -> Self {
+		todo!()
+	}
+
+	pub fn declare(ident: &Ident, value: Value, is_mut: bool) -> RuntimeResult<()> {
+        todo!()
+    }
+
+	pub fn assign(ident: &Ident, value: Value) -> RuntimeResult<()> {
+        todo!()
+    }
+
+	pub fn load(ident: &Ident) -> RuntimeResult<Value> {
+        todo!()
+    }
+
+	pub fn call(capture: ArwFrame, parameters: Parameters, arguments: Arguments) {
+        todo!()
+    }
+
+	pub fn ret() {
+        todo!()
+    }
+
+	pub fn push_scope() {
+        todo!()
+    }
+
+	pub fn pop_scope() {
+        todo!()
+    }
 }
