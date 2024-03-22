f7383603d1f3c6f8838ba05180a1489ae2499fa6
Abiria <abiria.dev@gmail.com>
2024-03-22 14:00:19 +0900

    docs: add `Playground` component

---
+++ docs/components/Playground.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+	import Split from './Split.vue'
+</script>
+
+<template>
+	<div :class="$style.main">
+		<header>Header</header>
+		<Split :class="$style.sp">
+			<template #left>
+				<div :class="$style.a">A</div>
+			</template>
+			<template #right>
+				<div :class="$style.b">B</div>
+			</template>
+		</Split>
+	</div>
+</template>
+
+<style module>
+	.main {
+		width: 100%;
+		height: calc(100vh - 64px);
+		display: flex;
+		flex-direction: column;
+	}
+
+	.sp {
+		outline: 1px solid red;
+		flex: 1;
+	}
+
+	.a,
+	.b {
+		height: 100%;
+	}
+</style>
