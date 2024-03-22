19c54dcbda903d0a358fbb7cd579e859efe06c1f
Abiria <abiria.dev@gmail.com>
2024-03-22 15:56:58 +0900

    feat: separate compiler and session

--- crates/interfaces/src/lib.rs
+++ crates/interfaces/src/lib.rs
@@ -1,4 +1,10 @@
-pub struct CompilerConfig {}
+use std::io::{Read, Write};
+
+pub struct CompilerConfig {
+	std: (),
+}
+
+impl CompilerConfig {}
 
 pub struct Compiler {}
 
@@ -6,4 +12,32 @@ impl Compiler {
 	pub fn new(config: CompilerConfig) -> Self {
 		todo!()
 	}
+
+	pub fn session<S, I, O, E>(
+		&self,
+		config: SessionConfig<S, I, O, E>,
+	) -> Session
+	where
+		S: Read,
+		I: Read,
+		O: Write,
+		E: Write,
+	{
+		todo!()
+	}
 }
+
+pub struct SessionConfig<S, I, O, E>
+where
+	S: Read,
+	I: Read,
+	O: Write,
+	E: Write,
+{
+	source: S,
+	stdin: I,
+	stdout: O,
+	stderr: E,
+}
+
+pub struct Session {}
